9161b3a0ff96145e950384b2ba265190db4ea525
Mussie <mussieabraham03@gmail.com>
2023-11-25 01:28:42 -0500

    setting up home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import {
-  MDBContainer,
-  MDBNavbar,
-  MDBNavbarBrand
-} from 'mdb-react-ui-kit';
+import React from "react";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import './App.css'; 
 
-export default function App() {
+function App() {
   return (
-    <>
-      <MDBNavbar light bgColor='light'>
-        <MDBContainer fluid>
-          <MDBNavbarBrand tag="span" className='mb-0 h1'>Navbar</MDBNavbarBrand>
-        </MDBContainer>
-      </MDBNavbar>
-    </>
+    <div className="App">
+      <div className="App-header">
+        <h1 className="App-title">SoundScape AI</h1>
+        <h2 className="App-subtitle">Where artificial intelligence meets your music taste.</h2>
+        <p className="Quote"> - Rash (2023)</p>
+      </div>
+    </div>
   );
 }
+
+export default App;
+
